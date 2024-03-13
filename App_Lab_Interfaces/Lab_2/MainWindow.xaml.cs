e5b1ade410379d89ca13a500f81ae018af01e141
DanilOchka <danilogolosniy@gmail.com>
2024-03-13 03:55:20 +0200

    Lab_2_tack#4

--- App_Lab_Interfaces/Lab_2/MainWindow.xaml.cs
+++ App_Lab_Interfaces/Lab_2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,19 +16,59 @@ using System.Windows.Shapes;
 
 namespace App_Lab_Interfaces.Lab_2
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static RoutedCommand ClearTextCommand = new RoutedCommand();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBinding saveCommand = new CommandBinding(ApplicationCommands.Save, execute_Save, canExecute_Save);
+            CommandBindings.Add(saveCommand);
+
+            CommandBinding clearTextCommand = new CommandBinding(ClearTextCommand, execute_ClearText, canExecute_ClearText);
+            CommandBindings.Add(clearTextCommand);
+
+            // Додайте обробник події для кнопки "Files"
+            filesButton.Click += OpenFileButton_Click;
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Зчитуємо вміст файлу і встановлюємо його у текстове поле
+                inputTextBox.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void canExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length > 0) e.CanExecute = true; else e.CanExecute = false;
+        }
+
+        private void execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText("e:\\Учеба\\3 курс\\2\\Інтерфейси\\Lab\\myFile.txt", inputTextBox.Text);
+            MessageBox.Show("The file was saved!");
+        }
+
+        private void canExecute_ClearText(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true; // Дозволяємо виконання команди
+        }
+
+        private void execute_ClearText(object sender, ExecutedRoutedEventArgs e)
+        {
+            inputTextBox.Text = string.Empty; // Очищаємо текстове поле
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
         }
     }
 }
